6e43626ffcc6a9b296e69e9407d0c93d4b0518a9
shokhmatic <haha@shokhmatic.shokh>
2023-06-14 23:56:59 +0330

    call get all images in main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from utils import get_recent_images
+from utils import get_all_images
 from dotenv import load_dotenv
+
+
+
 if __name__=='__main__':
     load_dotenv()
-    get_recent_images()
+    get_all_images()
